67163f95e353b7bb9b46bdbbd35b3413c4d6a7ac
SergiiSiniavin <siniavin@urancompany.com>
2025-05-28 16:56:01 +0300

    Finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://shimmering-twilight-05417e.netlify.app"
+  site: "https://shimmering-twilight-05417e.netlify.app",
+  integrations: [preact()]
 });
